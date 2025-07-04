02d1e77bc128c0de4f525dbc025856b2c45db468
benjamin kubricky <bkubricky@gmail.com>
2025-07-03 18:30:12 -0700

    useState todo app

--- todo-app/todo-app/src/components/MyList.jsx
+++ todo-app/todo-app/src/components/MyList.jsx
@@ -1,16 +1,36 @@
-import todoData from "../../data/todoData";
-import CheckListItem from "./CheckListItem";
+import React, { useState } from "react";
 
-function MyList() {
-  const myComponents = todoData.map(({ id, text, completed }) => (
-    <CheckListItem key={id} text={text} completed={completed} />
-  ));
+const initialTodos = [
+  { id: 1, text: "Take out trash", completed: false },
+  { id: 2, text: "Complete Homework", completed: true },
+  { id: 3, text: "Make bed", completed: false },
+  { id: 4, text: "Brush teeth", completed: true },
+];
+
+function TodoList() {
+  const [todos, setTodos] = useState(initialTodos);
+
   return (
-    <>
-      <h1>Todo List</h1>
-      <p>This will be a todo list</p>
-      <ul>{myComponents}</ul>
-    </>
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <span>{todo.text}</span>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => toggleTodo(todo.id)}
+          />
+        </div>
+      ))}
+    </div>
   );
+
+  function toggleTodo(id) {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  }
 }
-export default MyList;
+
+export default TodoList;
